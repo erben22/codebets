72ab046a0e50961c77f7b9584d5354c87224846b
R. Cody Erben <erben22@mtnaircomputer.net>
2019-07-04 12:40:25 -0600

    Adding latest stuff

---
+++ projects/terrapin/Terrapin/Terrapin-UnitTests/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Terrapin-UnitTests
+//
+//  Created by R. Cody Erben on 05/25/2013.
+//  Copyright (c) 2013 CodeBets. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!, gtest style\n";
+    return 0;
+}
+
---
+++ projects/terrapin/Terrapin/Terrapin/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Terrapin
+//
+//  Created by R. Cody Erben on 05/25/2013.
+//  Copyright (c) 2013 CodeBets. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
